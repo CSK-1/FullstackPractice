1289204b46d1396c5c1266f536187ec16d42563c
Chase K <chasekmbll@gmail.com>
2025-02-05 20:28:42 -0600

    Adding block_07 work

---
+++ block_07/vault.js
@@ -0,0 +1,13 @@
+// 1. Create a string for the user saying: You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+// 2. Assign a variable with an arithmetic equation for 10
+// 3. Assign a variable with an arithmetic equation for 40
+// 4. Assign a variable with an arithmetic equation for 39
+// 5. Create a popup dialog displaying the vault codes
+
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+
+const var1 = 20 / 2
+const var2 = 4 * 10
+const var3 = 40 - 1
+
+alert(`${message} ${var1} - ${var2} - ${var3}`)
